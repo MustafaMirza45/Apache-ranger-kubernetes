6ff5057cf2a2b7cac115ec2f88f3fc9f5be223ab
MustafaMirza45 <mustafamirza45@gmail.com>
2022-10-09 21:05:09 +0500

    latest

--- ranger-admin/docker-entrypoint.sh
+++ ranger-admin/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 RANGER_VERSION=2.1.0
 
-#wget https://github.com/aakashnand/trino-ranger-demo/releases/download/trino-ranger-demo-v1.0/ranger-${RANGER_VERSION}-admin.tar.gz && \
+#wget https://github.com/MustafaMirza45/Apache-ranger-kubernetes/releases/download/v1.0/ranger-${RANGER_VERSION}-admin.tar.gz && \
 tar xvf ranger-${RANGER_VERSION}-admin.tar.gz && \
 cd /root/ranger-${RANGER_VERSION}-admin/ && \
 cp /root/install.properties /root/ranger-${RANGER_VERSION}-admin/ && \
--- trino/docker-entrypoint.sh
+++ trino/docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 RANGER_VERSION=2.3.0
-# curl https://github.com/aakashnand/trino-ranger-demo/releases/download/trino-ranger-demo-v1.0/ranger-${RANGER_VERSION}-trino-plugin.tar.gz --output ranger-${RANGER_VERSION}-trino-plugin.tar.gz -L && \
-# tar xvf ranger-${RANGER_VERSION}-trino-plugin.tar.gz && \
-# cp -rf /install.properties /ranger-${RANGER_VERSION}-trino-plugin/ && \
 chown root:root -R /ranger-${RANGER_VERSION}-trino-plugin/* && \
 /ranger-${RANGER_VERSION}-trino-plugin/enable-trino-plugin.sh
 # /usr/lib/trino/bin/run-trino
